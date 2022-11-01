1c3e37cfc73de1963e2e55b3ce8ee81fe385e260
alexsoler9 <alex.soler.baca@estudiantat.upc.edu>
2022-11-01 18:39:44 +0100

    Commit final

--- src/vad_docopt.h
+++ src/vad_docopt.h
@@ -37,10 +37,10 @@ const char help_message[] =
 "   -i FILE, --input-wav=FILE   WAVE file for voice activity detection\n"
 "   -o FILE, --output-vad=FILE  Label file with the result of VAD\n"
 "   -w FILE, --output-wav=FILE  WAVE file with silences cleared\n"
-"   -1 REAL, --alfa1=REAL   Incremento del umbral 1 [default: 3]\n"
+"   -1 REAL, --alfa1=REAL   Incremento del umbral 1 [default: 2]\n"
 "   -v, --verbose  Show debug information\n"
 "   -h, --help     Show this screen\n"
-"   --version      Show the version of the project";
+"   --version      Show the version of the project\n";
 
 const char usage_pattern[] =
 "Usage:\n"
@@ -303,7 +303,7 @@ int elems_to_args(Elements *elements, DocoptArgs *args, bool help,
 
 DocoptArgs docopt(int argc, char *argv[], bool help, const char *version) {
     DocoptArgs args = {
-        0, 0, 0, (char*) "3", NULL, NULL, NULL,
+        0, 0, 0, (char*) "2", NULL, NULL, NULL,
         usage_pattern, help_message
     };
     Tokens ts;
